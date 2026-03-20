cafe821234907d6f77349641d8fe4ea749d2d337
Stacey Spears <stacey.spears@mykenzie.snhu.edu>
2026-03-20 00:46:55 -0500

    Refactor CustomerTypeConverter for AWS SDK v2

--- Application/src/main/java/com/kenzie/appserver/repositories/model/CustomerTypeConverter.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/CustomerTypeConverter.java
@@ -1,35 +1,48 @@
 package com.kenzie.appserver.repositories.model;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
-import com.kenzie.appserver.service.EventService;
 import com.kenzie.appserver.service.model.Customer;
-import com.kenzie.appserver.service.model.User;
+import software.amazon.awssdk.enhanced.dynamodb.AttributeConverter;
+import software.amazon.awssdk.enhanced.dynamodb.AttributeValueType;
+import software.amazon.awssdk.enhanced.dynamodb.EnhancedType;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class CustomerTypeConverter implements DynamoDBTypeConverter<List<String>, List<Customer>> {
+public class CustomerTypeConverter implements AttributeConverter<List<Customer>> {
 
     @Override
-    public List<String> convert(List<Customer> object) {
-
-        return object.stream().map(n -> String.format("%s x %s x %s", n.getId(), n.getName(), n.getEmail()))
+    public AttributeValue transformFrom(List<Customer> input) {
+        if (input == null) {
+            return AttributeValue.builder().nul(true).build();
+        }
+        List<AttributeValue> items = input.stream()
+                .map(n -> AttributeValue.builder()
+                        .s(String.format("%s x %s x %s", n.getId(), n.getName(), n.getEmail()))
+                        .build())
                 .collect(Collectors.toList());
+        return AttributeValue.builder().l(items).build();
     }
 
     @Override
-    public List<Customer> unconvert(List<String> object) {
-
-        return object.stream().map(n -> {
+    public List<Customer> transformTo(AttributeValue input) {
+        return input.l().stream().map(av -> {
             Customer customer = new Customer();
-            String[] data = n.split("x");
+            String[] data = av.s().split("x");
             customer.setId(data[0].trim());
             customer.setName(data[1].trim());
             customer.setEmail(data[2].trim());
             return customer;
-        })
-            .collect(Collectors.toList());
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    public EnhancedType<List<Customer>> type() {
+        return EnhancedType.listOf(Customer.class);
+    }
+
+    @Override
+    public AttributeValueType attributeValueType() {
+        return AttributeValueType.L;
     }
 }
-
