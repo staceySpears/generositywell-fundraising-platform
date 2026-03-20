53f7172a59917cb463ca8197a9a8e747be618bf5
Stacey Spears <stacey.spears@mykenzie.snhu.edu>
2026-03-20 00:04:04 -0500

    Refactor DynamoDbConfig to use AWS SDK v2

--- Application/src/main/java/com/kenzie/appserver/config/DynamoDbConfig.java
+++ Application/src/main/java/com/kenzie/appserver/config/DynamoDbConfig.java
@@ -1,36 +1,42 @@
 package com.kenzie.appserver.config;
 
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.net.URI;
 
 @Configuration
-@EnableDynamoDBRepositories(basePackages = "com.kenzie.appserver.repositories")
 public class DynamoDbConfig {
-    @Value("${dynamodb.override_endpoint}")
-    String dynamoOverrideEndpoint;
 
     @Bean
+    @Primary
     @ConditionalOnProperty(name = "dynamodb.override_endpoint", havingValue = "true")
-    public AmazonDynamoDB amazonDynamoDB(@Value("${dynamodb.endpoint}") String dynamoEndpoint) {
-        AwsClientBuilder.EndpointConfiguration endpointConfig = new
-                AwsClientBuilder.EndpointConfiguration(dynamoEndpoint,
-                "us-east-1");
-
-        return AmazonDynamoDBClientBuilder
-                .standard()
-                .withEndpointConfiguration(endpointConfig)
+    public DynamoDbClient localDynamoDbClient(@Value("${dynamodb.endpoint}") String dynamoEndpoint) {
+        return DynamoDbClient.builder()
+                .endpointOverride(URI.create(dynamoEndpoint))
+                .region(Region.US_EAST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
-
     }
 
-    @Bean(name = "amazonDynamoDB")
-    public AmazonDynamoDB defaultAmazonDynamoDb() {
-        return AmazonDynamoDBClientBuilder.defaultClient();
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    @Bean
+    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(dynamoDbClient)
+                .build();
     }
 }
