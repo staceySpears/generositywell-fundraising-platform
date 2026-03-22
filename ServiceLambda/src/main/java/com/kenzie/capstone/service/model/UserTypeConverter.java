acd43c6a0488ac4d49654c04a258a09d2c778e1d
Stacey Spears <stacey.spears@mykenzie.snhu.edu>
2026-03-21 23:25:15 -0500

    refactor: migrate UserTypeConverter to AWS SDK v2 AttributeConverter

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/model/UserTypeConverter.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/model/UserTypeConverter.java
@@ -1,38 +1,53 @@
 package com.kenzie.capstone.service.model;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
+import software.amazon.awssdk.enhanced.dynamodb.AttributeConverter;
+import software.amazon.awssdk.enhanced.dynamodb.AttributeValueType;
+import software.amazon.awssdk.enhanced.dynamodb.EnhancedType;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
-public class UserTypeConverter implements DynamoDBTypeConverter<String, User> {
-
-    @Override
-    public String convert(User object) {
-        User user = (User) object;
-        String users = null;
-        try {
-            if (user != null) {
-                users = String.format("%s x %s x %s", user.getId(), user.getName(),
-                        user.getEmail());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return users;
-    }
+/**
+ * AWS SDK v2 AttributeConverter for the User domain object.
+     * Serializes User to/from a single DynamoDB String attribute
+     * using the format: "id x name x email".
+     *
+     * Replaces the legacy SDK v1 DynamoDBTypeConverter implementation.
+     */
+public class UserTypeConverter implements AttributeConverter<User> {
 
     @Override
-    public User unconvert(String s) {
-
-        User user = new User();
-        try {
-            if (s != null && s.length() != 0) {
-                String[] data = s.split("x");
-                user.setId(data[0].trim());
-                user.setName(data[1].trim());
-                user.setEmail(data[2].trim());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        public AttributeValue transformFrom(User user) {
+                    if (user == null) {
+                                    return AttributeValue.builder().nul(true).build();
+                    }
+                    String value = String.format("%s x %s x %s",
+                                                                 user.getId(), user.getName(), user.getEmail());
+                    return AttributeValue.builder().s(value).build();
         }
-        return user;
-    }
+
+    @Override
+        public User transformTo(AttributeValue attributeValue) {
+                    User user = new User();
+                    try {
+                                    String s = attributeValue.s();
+                                    if (s != null && !s.isEmpty()) {
+                                                        String[] data = s.split(" x ");
+                                                        user.setId(data[0].trim());
+                                                        user.setName(data[1].trim());
+                                                        user.setEmail(data[2].trim());
+                                    }
+                    } catch (Exception e) {
+                                    e.printStackTrace();
+                    }
+                    return user;
+        }
+
+    @Override
+        public EnhancedType<User> type() {
+                    return EnhancedType.of(User.class);
+        }
+
+    @Override
+        public AttributeValueType attributeValueType() {
+                    return AttributeValueType.S;
+        }
 }
